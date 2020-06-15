0725d24a99c119950b58af8e3c52b402a1f7faed
Jordan <jordan.salm@gmail.com>
2020-06-15 14:21:46 -0700

    Create toolchain.cmake

---
+++ toolchain.cmake
@@ -0,0 +1,17 @@
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+
+
+set(CMAKE_SYSROOT /home/docker/rpi-rootfs)
+set(CMAKE_STAGING_PREFIX /home/docker/stage)
+
+set(tools /home/docker/toolchain-raspbian-buster-0-2-g62cd2c5-x86_64-arm-linux-gnueabihf)
+SET(CMAKE_C_COMPILER ${tools}/bin/arm-unknown-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER ${tools}/bin/arm-unknown-linux-gnueabi-g++)
+SET(CMAKE_FIND_ROOT_PATH ${tools})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
