296bff271f968f7c095871bf22c3cc67156645c1
Jordan <jordan.salm@gmail.com>
2020-06-15 14:33:02 -0700

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -5,7 +5,7 @@ SET(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSROOT /home/docker/rpi-rootfs)
 set(CMAKE_STAGING_PREFIX /home/docker/stage)
 
-set(tools /home/docker/toolchain-raspbian-buster-0-2-g62cd2c5-x86_64-arm-linux-gnueabihf)
+set(tools /home/docker/rpi-toolchain/toolchain-raspbian-buster-0-2-g62cd2c5-x86_64-arm-linux-gnueabihf)
 SET(CMAKE_C_COMPILER ${tools}/bin/arm-unknown-linux-gnueabi-gcc)
 SET(CMAKE_CXX_COMPILER ${tools}/bin/arm-unknown-linux-gnueabi-g++)
 SET(CMAKE_FIND_ROOT_PATH ${tools})
